2b95faa52b99d0ad1e7769a3eb8e976dd0966a63
lm199888 <3373330965@qq.com>
2023-03-11 20:45:14 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,10 @@
+from playwright.sync_api import Page,expect
+import re
+# 无头模式运行 不会打开浏览器，控制台输出执行结果日志
+def test_baidu(page:Page):
+    page.goto("http://www.baidu.com")
+    expect(page).to_have_title(re.compile("百度一下"))
+    input_text = page.locator("#kw")
+    input_text.fill("搜索关键字")
+    get_search = page.locator("text=百度一下")
+    get_search.click()
